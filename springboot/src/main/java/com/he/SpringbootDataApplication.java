2f6cb1f8f7f28d768035c1b7344f9126a7a7fa2d
hejinyu <hejinyu@rongcloud.cn>
2021-02-23 14:17:25 +0800

    springboot  加载步骤

---
+++ springboot/src/main/java/com/he/SpringbootDataApplication.java
@@ -0,0 +1,12 @@
+package com.he;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SpringbootDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDataApplication.class,args);
+    }
+}
